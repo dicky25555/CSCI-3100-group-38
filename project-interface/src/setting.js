8224bbeb90d0edbb787ba119e60cd3ab92522c1b
Phon423 <50314515+Phon423@users.noreply.github.com>
2020-04-22 23:46:07 +0800

    Update setting.js

--- project-interface/src/setting.js
+++ project-interface/src/setting.js
@@ -59,6 +59,59 @@ class setting extends React.Component{
 		}
     }
 	
+	deleteAccount = (e) =>{
+		var box = document.getElementById("deleteAccount");
+		
+		if (box.style.display === "none") {
+			box.style.display = "block";
+		} else {
+			box.style.display = "none";
+		}
+    }
+	
+	validateSignUp(value){
+        var inputFirstName = document.getElementById("fname");
+        var inputLastName = document.getElementById("lname");
+        var inputPassword = document.getElementById("pwd");
+        var inputPasswordConfirm = document.getElementById("pwdConfirm");
+
+		if(value == 1){        
+			if(!inputFirstName.checkValidity()){
+            alert("Check your first name !");
+			} else if(!inputLastName.checkValidity()){
+				alert("Check your last name !");
+			} else
+				this.state.formValid = true;
+		}else if(value == 2){ 
+			if(!inputPassword.checkValidity()){
+				alert("Check your password, make sure it is at least 8 characters !");
+			} else {
+				this.state.formValid = true;
+			}
+		}else if(value == 3){ 
+			if(!inputPasswordConfirm.checkValidity()){
+				alert("Check your password, make sure it is at least 8 characters !");
+			}  else {
+				this.state.formValid = true;
+			}
+		}
+    }
+	
+    onSubmit = (e, value) =>{
+        e.preventDefault();
+        this.validateSignUp(value);
+        if(this.state.formValid){
+			if(value == 1){
+				
+			}else if(value == 2){
+				
+			}else if(value == 3){
+				
+			}
+        }else{
+            alert("Please check your form again!");
+        }
+    }
 	
     render(){
         return(
@@ -81,6 +134,10 @@ class setting extends React.Component{
 							<td>
 								<input value={this.state.secondname} onChange={e => this.handleChange(e)} className="inputStyle3" id="lname" name="lastname" placeholder="Last Name" required/>
 							</td>
+							<td width="1%"></td>
+							<td>
+								<input onClick={e => this.onSubmit(e, 1)} type="submit" value="Confirm"/>
+							</td>
 						</tr>
                         <br/><br/>
                         <p class="header" style={{paddingBottom:"40px", paddingRight:"20px", borderBottom:"1px solid #ddd" , cursor: "pointer"}} onClick={e => this.passwordChange(e)} >Change Password </p>
@@ -88,9 +145,20 @@ class setting extends React.Component{
 							<td colspan="3">
 								<input value={this.state.password} onChange={e => this.handleChange(e)} type="password" className="inputStyle1" id="pwd" name="password" placeholder="New password" pattern=".{8,}" required/>
 							</td>
+							<td>
+								<input onClick={e => this.onSubmit(e, 2)} type="submit" value="Confirm"/>
+							</td>
 						</tr>
                         <br/><br/>
-                        <p class="header" style={{paddingBottom:"40px", paddingRight:"20px", borderBottom:"1px solid #ddd" , cursor: "pointer"}} >Delete Account </p>
+                        <p class="header" style={{paddingBottom:"40px", paddingRight:"20px", borderBottom:"1px solid #ddd" , cursor: "pointer"}} onClick={e => this.deleteAccount(e)} >Delete Account </p>
+						<tr id="deleteAccount" style={{display:"none"}}>
+							<td colspan="3">
+								<input value={this.state.password} onChange={e => this.handleChange(e)} type="password" className="inputStyle1" id="pwdConfirm" name="password" placeholder="Confirm your password" pattern=".{8,}" required/>
+							</td>
+							<td>
+								<input onClick={e => this.onSubmit(e, 3)} type="submit" value="Confirm"/>
+							</td>
+						</tr>
                         <br/><br/>
                         <p class="header" style={{paddingBottom:"40px", paddingRight:"20px", borderBottom:"1px solid #ddd" , cursor: "pointer"}} onClick={e => this.logOut(e)}>Log out </p>
                         
